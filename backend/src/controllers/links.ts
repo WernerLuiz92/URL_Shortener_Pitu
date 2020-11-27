e07823fee78bbea3948a0e53817cda04e40a68b3
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-11-27 20:58:14 -0300

    Implementação Funcionalidades
    - Implementação da rota geLink:
         Onde ele pega o código do link curto e procura a URL original correspondente, exibindo os status do link.
    
    - Implementação da rota hitLink:
         Onde ele pega o código e verifica a URL correspondente, incrementando a variavel hits para contabilizar os acessos.

--- backend/src/controllers/links.ts
+++ backend/src/controllers/links.ts
@@ -1,4 +1,5 @@
 import {Request,Response} from 'express';
+import { IndentStyle } from 'typescript';
 import {Link} from '../models/link';
 
 const links : Link[] = [];
@@ -23,11 +24,24 @@ function postLink(req: Request, res: Response){
 }
 
 function getLink(req: Request, res: Response){
-    res.send('getLink');
+    const code = req.params.code as string;
+    const link = links.find(item => item.code === code);
+    if(!link)
+        res.sendStatus(404);
+    else
+        res.json(link);
 }
 
 function hitLink(req: Request, res: Response){
-    res.send('hitLink');
+    const code = req.params.code as string;
+    const index = links.findIndex(item => item.code === code);
+
+    if(index === -1)
+        res.sendStatus(404);
+    else {
+        links[index].hits!++;
+        res.json(links[index]);
+    }
 }
 
 export default {
