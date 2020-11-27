8bd3b7ac9ae15d492af4da49d46c1a237f4fbb15
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-11-27 00:53:47 -0300

    Gerar Código Curto
    Implementamos a function que gera um código curto unico e aleatório, dentre os caracteres possiveis, e passamos este código para a function potLink.

--- backend/src/controllers/links.ts
+++ backend/src/controllers/links.ts
@@ -1,7 +1,25 @@
 import {Request,Response} from 'express';
+import {Link} from '../models/link';
+
+const links : Link[] = [];
+let proxId = 1;
+
+function generateCode(){
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for(let i=0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    
+    return text;
+}
 
 function postLink(req: Request, res: Response){
-    res.send('postLink');
+    const link = req.body as Link;
+    link.id = proxId++;
+    link.code = generateCode();
+    link.hits = 0;
+    links.push(link);
+    res.status(201).json(link);
 }
 
 function getLink(req: Request, res: Response){
