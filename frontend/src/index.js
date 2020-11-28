b6a54bf95cc75605b1e87081f357dacbbae1f45f
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-11-28 20:36:23 -0300

    Font Awesome

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './App.scss';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCheckSquare);
+
 
 ReactDOM.render(
   <React.StrictMode>
