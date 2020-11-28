d1b92d11bffa1ed8568e9ff881f9fe2283200be6
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-11-27 22:58:16 -0300

    Update links.ts
    Atualizamos as functions para enviar os dados para o Banco de Dados.

--- backend/src/controllers/links.ts
+++ backend/src/controllers/links.ts
@@ -1,9 +1,7 @@
 import {Request,Response} from 'express';
-import { IndentStyle } from 'typescript';
 import {Link} from '../models/link';
+import linksRepository from '../models/linksRepository';
 
-const links : Link[] = [];
-let proxId = 1;
 
 function generateCode(){
     let text = '';
@@ -14,33 +12,35 @@ function generateCode(){
     return text;
 }
 
-function postLink(req: Request, res: Response){
+async function postLink(req: Request, res: Response){
     const link = req.body as Link;
-    link.id = proxId++;
     link.code = generateCode();
     link.hits = 0;
-    links.push(link);
+    const result = await linksRepository.add(link);
+    if(!result.id) return res.sendStatus(400);
+
+    link.id = result.id!;
+
     res.status(201).json(link);
 }
 
-function getLink(req: Request, res: Response){
+async function getLink(req: Request, res: Response){
     const code = req.params.code as string;
-    const link = links.find(item => item.code === code);
+    const link = await linksRepository.findByCode(code);
     if(!link)
         res.sendStatus(404);
     else
         res.json(link);
 }
 
-function hitLink(req: Request, res: Response){
+async function hitLink(req: Request, res: Response){
     const code = req.params.code as string;
-    const index = links.findIndex(item => item.code === code);
+    const link = await linksRepository.hit(code);
 
-    if(index === -1)
+    if(!link)
         res.sendStatus(404);
     else {
-        links[index].hits!++;
-        res.json(links[index]);
+        res.json(link);
     }
 }
 
