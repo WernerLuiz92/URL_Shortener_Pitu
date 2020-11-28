50c9f49401fa4c9e02082c05836b8493ecb52bca
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-11-27 21:57:39 -0300

    Startando a conexão com o DB
    Aqui iniciamos a conexão com o Banco de Dados

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,9 @@
 import app from './app';
+import database from './database';
+
+database.sync({force: true});
+console.log('Database running at 3306');
+
 
 app.listen(3000);
 console.log('Server running at 3000');
