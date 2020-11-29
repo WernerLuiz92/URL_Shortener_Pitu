d97efda4f14688727c3b00f5d24644abb7f75c80
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-11-29 03:28:33 -0300

    Redirect Page
    Criamos o arquivo de estilos para a Redirect Page, e configuramos o tratamento de erros para ficar amigavel ao usuário.

--- frontend/src/pages/RedirectPage/index.js
+++ frontend/src/pages/RedirectPage/index.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import { render } from 'react-dom';
+import Header from '../../components/Header';
+import { Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { StatsContainer } from './styles';
+
+import ShortenerService from '../../services/shortenerService';
 
 class RedirectPage extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            isLoading: false,
+            url: '',
+            errorMessage: '',
+        }
+    }
+
+    async componentDidMount() {
+        const { code } = this.props.match.params;
+
+        try {
+            const service = new ShortenerService();
+            const { url } = await service.getLink(code);
+
+            window.location = url;
+        } catch (error) {
+            this.setState({ isLoading: false, errorMessage: 'Ops, a URL solicitada não existe.'})
+        }
     }
 
     render() {
+        const { errorMessage } = this.state;
+
         return (
-            <p>Pitu - Redirect</p>
+            <Container>
+                {errorMessage ? (
+                    <>
+                        <Header>Seu novo encurtador de URLs <FontAwesomeIcon size="2x" icon={["fab", "angellist"]} /></Header>
+                        <StatsContainer className="text-center">
+                            <FontAwesomeIcon size="3x" color="#f8d7da" icon="exclamation-triangle" />
+                            <p className="m-3">{errorMessage}</p>
+                            <a className="btn btn-sm btn-primary" href="/">Encurtar Nova URL</a>
+                        </StatsContainer>
+                    </>
+                ) : (
+                    <p className="text-center">Redirecionando ... </p>
+                )}
+            </Container>
+
         )
     } 
 }
