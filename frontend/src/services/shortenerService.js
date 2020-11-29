1e6e9cf6d0995e99b1c56af10e05ffecfdf6b2eb
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-11-29 02:37:06 -0300

    Correção erro de digitação
    Aqui digitei ' ' no lugar de ` `

--- frontend/src/services/shortenerService.js
+++ frontend/src/services/shortenerService.js
@@ -7,19 +7,19 @@ class ShortenerService {
 
 
     async getLink(code) {
-        const result = await this.api.get('links/${code}');
+        const result = await this.api.get(`links/${code}`);
 
         return result.data;
     }
 
     async getStats(code) {
-        const result = await this.api.get('links/${code}/stats');
+        const result = await this.api.get(`links/${code}/stats`);
 
         return result.data;
     }
 
     async generate(model) {
-        const result = await this.api.post('links', model);
+        const result = await this.api.post(`links`, model);
 
         return result.data;
     }
