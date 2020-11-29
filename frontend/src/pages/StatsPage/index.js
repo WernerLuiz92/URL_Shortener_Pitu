bad74e45576896c4ce0661b9640eedaa0b65dece
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-11-29 01:30:55 -0300

    StatsPage
    Início da implementação das paginas de estatisticas.

--- frontend/src/pages/StatsPage/index.js
+++ frontend/src/pages/StatsPage/index.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import { render } from 'react-dom';
+import Header from '../../components/Header';
+import { Container } from 'react-bootstrap';
+
+import ShortenerService from '../../services/shortenerService';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { StatsContainer, StatsRow, StatsBox, StatsBoxTitle } from './styles';
 
 class StatsPage extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            isLoading: false,
+            shortenedURL: {},
+            errorMessage: '',
+        }
     }
 
     render() {
+        const { errorMessage, shortenedURL } = this.state;
+        
         return (
-            <p>Pitu - Stats</p>
+            <Container>
+                <Header>Estatísticas:</Header>
+                {errorMessage ? (
+                    <StatsContainer>
+                        <FontAwesomeIcon size="3x" color="#f8d7da" icon="exclamation-triangle" />
+                        <p className="m-3">{errorMessage}</p>
+                        <a className="btn btn-sm btn-primary" href="/">Encurtar Nova URL</a>
+                    </StatsContainer>
+
+                ) : (
+                    <p>Texto qualquer</p>
+                )}
+            </Container>
         )
     } 
 }
