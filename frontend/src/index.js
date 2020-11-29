9545b882fb6e449c9e2f271ceb09901094755da0
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-11-29 00:51:30 -0300

    FontAwesome Icons
    Corrigido problema do icon não aparecer.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './App.scss';
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheckSquare, faCompressAlt } from '@fortawesome/free-solid-svg-icons';
+import { faCopy } from '@fortawesome/free-regular-svg-icons';
 
-library.add(faCheckSquare);
+library.add(faCheckSquare, faCopy, faCompressAlt);
 
 
 ReactDOM.render(
